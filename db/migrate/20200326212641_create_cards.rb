183522e8c2a45b6082c0fb5f3aa9da231ae51b67
Scott Key <scottdkey@gmail.com>
2020-03-26 15:45:58 -0600

    init

---
+++ db/migrate/20200326212641_create_cards.rb
@@ -0,0 +1,13 @@
+class CreateCards < ActiveRecord::Migration[6.0]
+  def change
+    create_table :cards do |t|
+      t.string :name
+      t.text :answers
+      t.boolean :complete
+      t.integer :points
+      t.belongs_to :category, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
