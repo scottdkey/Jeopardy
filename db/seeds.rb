1d758fa7a7493f69b0f7f59fb152fadfb8605406
Scott Key <scottdkey@gmail.com>
2020-03-27 17:15:58 -0600

    categories on the page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,3 +7,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Category.create(name: 'first', complete: false)
+Category.create(name: 'Second', complete: false)
+Category.create(name: 'Third', complete: true)
+Category.create(name: 'Fourth', complete: false)
+Category.create(name: '5th', complete: true)
+
+puts '5 categories created'
