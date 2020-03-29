684648326cd64dc1c9a60f4ce4073c84eb86f85b
Scott Key <scottdkey@gmail.com>
2020-03-29 13:11:55 -0600

    updated pull data mehtod

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import Categories from './components/categories';
@@ -6,13 +6,30 @@ import Categories from './components/categories';
 import CardForm from './components/CardForm'
 
 
-function App() {
-  return (
-    <div className="App">
-        <Categories />
-        <CardForm />
-    </div>
-  );
+class App extends Component {
+
+  pullData = () => {
+    axios
+      .get("api/categories")
+      .then(res => {
+        this.setState({
+          categories: res.data,
+          load: false
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Categories pullData={this.pullData}/>
+        <CardForm pullData={this.pullData}/>
+      </div>
+    );
+  }
 }
 
 export default App;
